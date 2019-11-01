95ba83d65c07f9feee4a2147e7a894e0f79423e3
Takuya Aso <milanista224@gmail.com>
2019-11-01 19:57:05 +0900

    [Model]Add StudyGroup model.

---
+++ StudyGroupEventFetcherForSwiftUI/Model/StudyGroup.swift
@@ -0,0 +1,13 @@
+//
+//  StudyGroup.swift
+//  StudyGroupEventFetcherForSwiftUI
+//
+//  Created by Takuya Aso on 2019/11/01.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import Foundation
+
+struct StudyGroup: Decodable {
+    var events: [Event]
+}
