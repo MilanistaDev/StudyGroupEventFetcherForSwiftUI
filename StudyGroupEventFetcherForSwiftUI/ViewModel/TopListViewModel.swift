b545ca54ca856f3413282b83103e5ea154b3c3c4
Takuya Aso <milanista224@gmail.com>
2019-11-05 18:56:59 +0900

    [MVVM]Use MVVM architecture.

---
+++ StudyGroupEventFetcherForSwiftUI/ViewModel/TopListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TopListViewModel.swift
+//  StudyGroupEventFetcherForSwiftUI
+//
+//  Created by Takuya Aso on 2019/11/05.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import Foundation
