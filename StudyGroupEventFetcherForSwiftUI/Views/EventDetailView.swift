63b1f2c6ce02dc6ac4ad9f4a437cbb051566f93c
Takuya Aso <milanista224@gmail.com>
2019-11-05 12:24:42 +0900

    [Detail]Implementation of Event Detail Screen View.

---
+++ StudyGroupEventFetcherForSwiftUI/Views/EventDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  EventDetailView.swift
+//  StudyGroupEventFetcherForSwiftUI
+//
+//  Created by Takuya Aso on 2019/11/05.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct EventDetailView: View {
+
+    @State var showModal = false
+
+    var eventData: Event    // From ListView
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center) {
+                Image("img_yumemi_swift").scaledToFit()
+                    .padding(Edge.Set.bottom, 8.0)
+                Text(eventData.title)
+                    .font(.headline)
+                    .lineLimit(2)
+                    .padding(Edge.Set.bottom, 16.0)
+                Button(action: {
+                    self.showModal.toggle()
+                }) {
+                    Text("connpassのイベントページへ")
+                        .font(.subheadline)
+                }
+            }
+            .navigationBarTitle("Event Detail")
+        }
+    }
+}
+
+struct EventDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventDetailView(eventData: mockEventsData[0])
+    }
+}
