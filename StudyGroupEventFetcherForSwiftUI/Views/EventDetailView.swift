8783c476a0a4c748421fc71d0c21440b448b4338
Takuya Aso <milanista224@gmail.com>
2019-12-22 23:46:20 +0900

    [Map]Add detail View Part and redesigned button.

--- StudyGroupEventFetcherForSwiftUI/Views/EventDetailView.swift
+++ StudyGroupEventFetcherForSwiftUI/Views/EventDetailView.swift
@@ -11,24 +11,28 @@ import MapKit
 
 struct EventDetailView: View {
 
-    @State var showModal = false
-
-    var eventData: Event    // From ListView
+    @State private var showModal = false
+    var eventData: Event    // From ListView(静的モデル)
 
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
+                // MapView Part
                 MapView().frame(height: 250)
-                Text(eventData.title)
-                    .font(.headline)
-                    .lineLimit(2)
-                    .padding(Edge.Set.bottom, 16.0)
+                // Event detail part
+                DetailPartView(eventData: self.eventData)
+                // Bottom button
                 Button(action: {
                     self.showModal.toggle()
                 }) {
-                    Text("connpassのイベントページへ")
-                        .font(.subheadline)
+                    Text("connpassのイベントページ")
+                        .font(Font.body.bold())
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(5.0)
                 }
+                .frame(minWidth: 0, maxWidth: .infinity)
                 .sheet(isPresented: $showModal) {
                     SafariView(url: URL(string: self.eventData.eventUrl))
                         .edgesIgnoringSafeArea(.bottom)
@@ -54,6 +58,41 @@ struct MapView: UIViewRepresentable {
 
     // Required
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+    }
+}
+
+struct DetailPartView: View {
+
+    var eventData: Event!
+
+    var body: some View {
+        Group{
+            Text(eventData.title)
+                .font(.headline)
+                .lineLimit(2)
+                .padding([.leading, .trailing], 20.0)
+                .padding(.bottom, 8.0)
+            Text(eventData.subTitle)
+                .font(.caption)
+                .foregroundColor(.gray)
+                .lineLimit(3)
+                .padding([.leading, .trailing], 20.0)
+                .padding(.bottom, 24.0)
+            Text(eventData.place)
+                .font(.headline)
+                .lineLimit(2)
+                .padding([.leading, .trailing], 20.0)
+                .padding(.bottom, 12.0)
+            Text(eventData.address)
+                .font(.footnote)
+                .lineLimit(2)
+                .padding([.leading, .trailing], 20.0)
+                .padding(.bottom, 24.0)
+            Text(StudyGroupDateFormatter.convertNormalDateString(dateStr: eventData.startDate, isOnlyDate: false) + " ~ " + StudyGroupDateFormatter.convertNormalDateString(dateStr: eventData.endDate, isOnlyDate: true))
+                .font(.subheadline)
+                .padding([.leading, .trailing], 20.0)
+                .padding(.bottom, 48.0)
+        }
     }
 }
 
