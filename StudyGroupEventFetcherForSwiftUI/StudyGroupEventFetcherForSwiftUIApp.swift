0dbe3fd66791fafd9e09a3560211bddfae1e3e23
Takuya Aso <milanista224@gmail.com>
2022-12-21 20:42:34 +0900

    #40 SwiftUI App のライフサイクルに変更

---
+++ StudyGroupEventFetcherForSwiftUI/StudyGroupEventFetcherForSwiftUIApp.swift
@@ -0,0 +1,9 @@
+//
+//  StudyGroupEventFetcherForSwiftUIApp.swift
+//  StudyGroupEventFetcherForSwiftUI
+//
+//  Created by Takuya Aso on 2022/12/21.
+//  Copyright © 2022 Takuya Aso. All rights reserved.
+//
+
+import Foundation
