1af8c6d07c1bcf932b4b65d686b4c217a35b7e8d
Takuya Aso <milanista224@gmail.com>
2022-12-21 14:16:41 +0900

    #39 APIError追加

---
+++ StudyGroupEventFetcherForSwiftUI/Other/APIError.swift
@@ -0,0 +1,30 @@
+//
+//  APIError.swift
+//  StudyGroupEventFetcherForSwiftUI
+//
+//  Created by Takuya Aso on 2022/12/21.
+//  Copyright © 2022 Takuya Aso. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case response
+    case jsonDecode
+    case statusCode(statusCode: String)
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .response:
+            return "Response Error"
+
+        case .jsonDecode:
+            return "json convert failed in JSONDecoder"
+
+        case .statusCode(let statusCode):
+            return "Error! StatuCode: " + String(statusCode)
+        }
+    }
+}
